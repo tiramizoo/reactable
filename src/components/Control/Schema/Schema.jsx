b3e6dacbe3198c51cac9e641ba27e7f1d115383b
Jakub Cieslar <cieslar.jakub@gmail.com>
2018-11-14 15:52:43 +0100

    cleanup

--- src/components/Control/Schema/Schema.jsx
+++ src/components/Control/Schema/Schema.jsx
@@ -2,22 +2,21 @@ import React from 'react'
 
 const Schema = (props) => {
   const handleChange = (e, key) => {
-    console.log("A: ", e.target.checked, key)
     const { schema, updateSchemaOptions } = props
     const { checked } = e.target
-    const newOptions =  Object.assign({}, schema[key], { hide: !checked })
+    const newOptions = Object.assign({}, schema[key], { hide: !checked })
     updateSchemaOptions(key, newOptions)
   }
 
   const renderElement = (key) => {
     const { schema } = props
     const schemaParams = schema[key]
-    const value = !schemaParams['hide']
+    const value = !schemaParams.hide
     return (
       <li>
         <input
           type="checkbox"
-          onChange={(e) => handleChange(e, key)}
+          onChange={e => handleChange(e, key)}
           checked={value}
         />
         {key}
@@ -29,8 +28,7 @@ const Schema = (props) => {
     <div>
       <ul>
         { Object.keys(props.schema).map(key =>
-          renderElement(key)
-        )}
+          renderElement(key)) }
       </ul>
     </div>
   )
