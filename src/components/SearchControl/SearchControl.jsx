925815fd319a645a7963201ee56697db7e8bc35c
Tomasz Mazur <defkode@gmail.com>
2018-12-03 11:50:25 +0100

    wip

--- src/components/SearchControl/SearchControl.jsx
+++ src/components/SearchControl/SearchControl.jsx
@@ -3,48 +3,15 @@ import SearchList from './SearchList'
 
 const SearchControl = (props) => {
   const {
-    updateViewport, filteredItems, controlShow,
-    setLimit, limit, setOffset, offset, tableWidth,
+    controlShow,
+    tableWidth,
   } = props
 
-  const updateLimit = (e) => {
-    const userInput = parseInt(e.target.value, 10)
-    const inputValueMin = 1 // min
-    const inputValueMax = filteredItems.length // max
-
-    const newLimit = (userInput > inputValueMax) ?
-      inputValueMax :
-      Math.max(inputValueMin, userInput)
-    const newOffset = 0
-
-    setOffset(newOffset)
-    setLimit(newLimit)
-
-    updateViewport(filteredItems, newLimit, newOffset)
-  }
-
-  const updateOffset = (e) => {
-    const userInput = parseInt(e.target.value, 10)
-
-    const inputValueMin = 0 // min
-    const inputValueMax = filteredItems.length - limit // max
-
-    const newOffset = (userInput > inputValueMax) ?
-      inputValueMax :
-      Math.max(inputValueMin, userInput)
-
-    setOffset(newOffset)
-    updateViewport(filteredItems, limit, newOffset)
-  }
   if (!controlShow) {
     return null
   }
   return (
     <div className="control" style={{width: tableWidth}}>
-      <div>
-        <span> Limit: <input onChange={updateLimit} value={limit} type="number" /></span>
-        <span> Offset: <input onChange={updateOffset} value={offset} type="number" /></span>
-      </div>
       <div>
         <SearchList />
       </div>
