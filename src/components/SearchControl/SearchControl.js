d18a71c5ab12c81200a5157e730f45a2e9bf271f
Jakub Cieslar <cieslar.jakub@gmail.com>
2018-12-04 15:19:45 +0100

    remove SearchList

--- src/components/SearchControl/SearchControl.js
+++ src/components/SearchControl/SearchControl.js
@@ -1,32 +1,17 @@
 import { connect } from 'react-redux'
 
 import SearchControlComponent from './SearchControl.jsx'
-import { setLimit, setOffset, updateViewport } from '../../actions/items'
+import { clearAllSearchQuery } from '../../actions/search'
 
 const mapStateToProps = state => ({
-  items: state.items,
-  currentItems: state.currentItems,
-  filteredItems: state.filteredItems,
-  limit: state.limit,
-  offset: state.offset,
   controlShow: state.settings.controlShow,
   tableWidth: state.table.width,
+  filteredSchema: state.filteredSchema,
 })
 
-const mapDispatchToProps = dispatch => (
-  {
-    setLimit: (limit) => {
-      dispatch(setLimit(limit))
-    },
-    setOffset: (offset) => {
-      dispatch(setOffset(offset))
-    },
-    updateViewport: (items, limit, offset) => {
-      dispatch(updateViewport(items, limit, offset))
-    },
-  }
-)
+const mapDispatchToProps = {
+  clearAllSearchQuery,
+}
 
 const SearchControl = connect(mapStateToProps, mapDispatchToProps)(SearchControlComponent)
-
 export default SearchControl
--- src/components/SearchControl/SearchControl.jsx
+++ src/components/SearchControl/SearchControl.jsx
@@ -1,23 +1,73 @@
-import React from 'react'
-import SearchList from './SearchList'
+import React, { Component } from 'react'
 
-const SearchControl = (props) => {
-  const {
-    controlShow,
-    tableWidth,
-  } = props
+import SearchText from './SearchText'
+import SearchBoolean from './SearchBoolean'
+import SearchNumber from './SearchNumber'
+import SearchDate from './SearchDate'
+import SearchDateTime from './SearchDateTime'
 
-  if (!controlShow) {
-    return null
+class SearchControl extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clearAll: 0,
+    }
   }
-  return (
-    <div className="control" style={{ width: tableWidth }}>
-      <div>
-        <SearchList />
-      </div>
 
-    </div>
-  )
+  getSearchInput(type, column) {
+    const { clearAll } = this.state
+    switch (type) {
+      case 'text':
+        return <SearchText column={column} key={clearAll} />
+      case 'boolean':
+        return <SearchBoolean column={column} key={clearAll} />
+      case 'number':
+        return <SearchNumber column={column} key={clearAll} />
+      case 'date':
+        return <SearchDate column={column} key={clearAll} />
+      case 'datetime':
+        return <SearchDateTime column={column} key={clearAll} />
+      case 'time':
+        return null
+      default:
+        return null
+    }
+  }
+
+  hideColumn(schemaParams) {
+    return (schemaParams['filter'] !== 'undefined' && schemaParams['filter'] === false)
+  }
+
+  columnFilter(key, schemaParams) {
+    if (this.hideColumn(schemaParams)) {
+      return null
+    }
+    return <div key={key}>{this.getSearchInput(schemaParams.type, key)}</div>
+  }
+
+  handleClearAllChange() {
+    const { clearAll } = this.state
+
+    this.setState({ clearAll: clearAll + 1 })
+    this.props.clearAllSearchQuery()
+  }
+
+  render() {
+    const { filteredSchema, controlShow, tableWidth } = this.props
+
+    if (!controlShow) {
+      return null
+    }
+
+    return (
+      <div className="control" style={{ width: tableWidth }}>
+        <div>
+          <button onClick={() => this.handleClearAllChange()}>Clear all</button>
+          { Object.entries(filteredSchema).map(([key, keySchema]) => this.columnFilter(key, keySchema))}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default SearchControl
