d64e9590f3da998b8c3cc856eb0f8cdc236dba6b
Jakub Cieslar <cieslar.jakub@gmail.com>
2018-12-05 20:34:13 +0100

    some tests

--- src/helpers/__utilities.test.js
+++ src/helpers/__utilities.test.js
@@ -1,6 +1,76 @@
-import { addZeroToNumber } from './utilities'
+import { addZeroToNumber, searchByBoolean } from './utilities'
 
 test('addZeroToNumber', () => {
   expect(addZeroToNumber(1)).toBe('01')
   expect(addZeroToNumber(10)).toBe('10')
+})
+
+// Search
+const items = [
+  {
+    id: 1,
+    first_name: 'Chloe',
+    last_name: 'Sipes',
+    date_of_birth: '2006-12-20',
+    solary: 305,
+    active: true,
+  },
+  {
+    id: 2,
+    first_name: 'Harrison',
+    last_name: 'Oga',
+    date_of_birth: '2000-10-20',
+    solary: 1900,
+    active: true,
+  },
+  {
+    id: 3,
+    first_name: null,
+    last_name: 'James',
+    date_of_birth: '1980-02-15',
+    solary: 3000,
+    active: null,
+  },
+  {
+    id: 4,
+    first_name: 'Lebron',
+    last_name: 'James',
+    date_of_birth: '1980-02-20',
+    solary: 3400,
+    active: false,
+  },
+  {
+    id: 5,
+    first_name: 'Luka',
+    last_name: 'Doncic',
+    date_of_birth: '1999-02-15',
+    solary: null,
+    active: null,
+  },
+  {
+    id: 6,
+    first_name: 'Marcin',
+    last_name: 'Gortat',
+    date_of_birth: '1979-02-15',
+    solary: null,
+    active: true,
+  },
+]
+
+test('searchByBoolean', () => {
+  const column = 'active'
+  let searchQuery = { value: 'true' }
+  expect(searchByBoolean(items, column, searchQuery).length).toBe(3)
+
+  searchQuery = { value: 'false' }
+  expect(searchByBoolean(items, column, searchQuery).length).toBe(1)
+
+  searchQuery = { value: 'all' }
+  expect(searchByBoolean(items, column, searchQuery).length).toBe(6)
+
+  searchQuery = { value: 'empty' }
+  expect(searchByBoolean(items, column, searchQuery).length).toBe(2)
+
+  searchQuery = { value: 'notEmpty' }
+  expect(searchByBoolean(items, column, searchQuery).length).toBe(4)
 })
--- src/helpers/utilities.js
+++ src/helpers/utilities.js
@@ -3,6 +3,11 @@ import uniqueId from 'lodash/uniqueId'
 import map from 'lodash/map'
 import forEach from 'lodash/forEach'
 import compact from 'lodash/compact'
+import filter from 'lodash/filter'
+import isEmpty from 'lodash/isEmpty'
+import uniqBy from 'lodash/uniqBy'
+import concat from 'lodash/concat'
+import omit from 'lodash/omit'
 
 export function sortByType(items, action) {
   return orderBy(items, [action.column], [action.direction])
@@ -16,6 +21,12 @@ export function sortBy(items, filteredSchema) {
     }
   })
   return sortedItems
+}
+
+export function setSortDiractionToSchema(schema, key, direction) {
+  forEach(schema, (_, k) => { delete schema[k].direction })
+  const options = Object.assign({}, schema[key], { direction })
+  return Object.assign({}, schema, { [key]: options })
 }
 
 export function addMetaDataToItems(items) {
@@ -36,12 +47,6 @@ export function addParsedDateTime(items, schema) {
     })
     return Object.assign({}, item, ...newItem)
   })
-}
-
-export function setSortDiractionToSchema(schema, key, direction) {
-  forEach(schema, (_, k) => { delete schema[k].direction })
-  const options = Object.assign({}, schema[key], { direction })
-  return Object.assign({}, schema, { [key]: options })
 }
 
 export const addZeroToNumber = (value) => {
@@ -77,4 +82,144 @@ export const defaultFormatter = (type, key) => {
     default:
       return value => value
   }
+}
+
+// Search
+
+export function searchByText(items, column, searchQuery) {
+  const options = searchQuery.options || 'all'
+  switch (options) {
+    case 'all':
+      if (searchQuery.value) {
+        return filter(items, item => (
+          item[column] && item[column].toLowerCase().includes(searchQuery.value.toLowerCase())
+        ))
+      }
+      return items
+    case 'equal':
+      return filter(items, item => item[column] && item[column] === searchQuery.value)
+    case 'notEqual':
+      return filter(items, item => item[column] && item[column] !== searchQuery.value)
+    case 'match':
+      return filter(items, item => item[column] && item[column].match(searchQuery.value))
+    case 'notMatch':
+      return filter(items, item => item[column] && !item[column].match(searchQuery.value))
+    case 'empty':
+      return filter(items, item => isEmpty(item[column]))
+    case 'notEmpty':
+      return filter(items, item => !isEmpty(item[column]))
+    default:
+      return []
+  }
+}
+
+export function searchByBoolean(items, column, searchQuery) {
+  const options = searchQuery.value || 'all'
+  switch (options) {
+    case 'all':
+      return items
+    case 'true':
+      return filter(items, item => item[column] === true)
+    case 'false':
+      return filter(items, item => item[column] === false)
+    case 'empty':
+      return filter(items, item => item[column] !== true && item[column] !== false)
+    case 'notEmpty':
+      return filter(items, item => item[column] === true || item[column] === false)
+    default:
+      return []
+  }
+}
+
+export function searchByRange(items, column, searchQuery) {
+  if (searchQuery.value) {
+    if (searchQuery.value.from && searchQuery.value.to) {
+      return filter(items, (item) => {
+        const itemValue = item[column]
+        return itemValue >= searchQuery.value.from && itemValue <= searchQuery.value.to
+      })
+    }
+    if (searchQuery.value.from) {
+      return filter(items, item => item[column] >= searchQuery.value.from)
+    }
+    if (searchQuery.value.to) {
+      return filter(items, item => item[column] <= searchQuery.value.to)
+    }
+  }
+  return items
+}
+
+export function searchByType(items, type, column, searchQuery) {
+  switch (type) {
+    case 'text':
+      return searchByText(items, column, searchQuery)
+    case 'boolean':
+      return searchByBoolean(items, column, searchQuery)
+    case 'number':
+    case 'date':
+    case 'datetime':
+      return searchByRange(items, column, searchQuery)
+    default:
+      return []
+  }
+}
+
+export const searchByOr = (items, schema, searchQuery) => {
+  if (isEmpty(searchQuery)) {
+    return items
+  }
+
+  let filteredItems = []
+  Object.entries(searchQuery).map(([column, searchQueryValue]) => {
+    const newFilteredItems = searchByType(items, schema[column].type, column, searchQueryValue)
+    filteredItems = concat(filteredItems, newFilteredItems)
+    return filteredItems
+  })
+
+  return uniqBy(filteredItems, '_key')
+}
+
+export const searchByAnd = (items, schema, searchQuery) => {
+  if (isEmpty(searchQuery)) {
+    return items
+  }
+
+  let filteredItems = items
+  Object.entries(searchQuery).map(([column, searchQueryValue]) => {
+    filteredItems = searchByType(filteredItems, schema[column].type, column, searchQueryValue)
+    return filteredItems
+  })
+  return filteredItems
+}
+
+export const searchBy = (items, searchQueryAnd, searchQueryOr, schema, strategySearch) => {
+  if (isEmpty(searchQueryAnd) && isEmpty(searchQueryOr)) {
+    return items
+  }
+
+  if (strategySearch === 'or') {
+    const filteredItemsAnd = searchByAnd(items, schema, searchQueryAnd)
+    const filteredItemsOr = searchByOr(items, schema, searchQueryOr)
+
+    return uniqBy(concat(filteredItemsAnd, filteredItemsOr), '_key')
+  }
+  const filteredItemsAnd = searchByAnd(items, schema, searchQueryAnd)
+
+  return searchByOr(filteredItemsAnd, schema, searchQueryOr)
+}
+
+export const mergeSearchQuery = (newQuery, currentQuery) => {
+  let newSearchQuery = currentQuery
+
+  Object.entries(newQuery).forEach(([column, _searchQuery]) => {
+    const { value, options } = _searchQuery
+    if (value !== undefined || options !== undefined) {
+      const newSearchValue = Object.assign({}, newSearchQuery[column], { value, options })
+      newSearchQuery = Object.assign({}, newSearchQuery, { [column]: newSearchValue })
+    } else {
+      newSearchQuery = Object.assign({}, {}, omit(newSearchQuery, column))
+    }
+  })
+
+  return newSearchQuery
 }
